cb7385f170d4a85025027b07e185724513939f7b
marqbispo83 <marqbispo@gmail.com>
2025-09-27 16:22:25 +0000

    criando os movimentos das variaveis usando for, while, do while

--- xadrez.c
+++ xadrez.c
@@ -7,6 +7,35 @@
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    int torre = 5, rainha = 8, bispo = 5;   //variaveis e as quantidades de casas podem andar
+
+    printf("Movimento da Torre:\n");  //movimento da torre (Direita)
+    for (int i = 1; i <= torre; i++)  //usando for
+    {
+        printf("Direita.\n");
+    }
+
+    printf("Movimento do Bispo\n"); //movimento do Bispo (Cima / Direita)
+    int i = 1;
+
+    while (i <= bispo)  //usando while
+    {
+        printf("Cima direita\n");
+        i++;
+    }
+
+    printf("Movimento da Rainha\n");    //movimento da Rainha (Esquerda)
+    int r = 1;
+
+    do  //usando Do while
+    {
+        printf("Esquerda.\n");
+        r++;
+    } while (r <= rainha);
+    
+    
+    
+    
 
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
